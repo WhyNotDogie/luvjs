dab83b097f20e5bc83d1e73fde8974c0a85c243f
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2023-03-04 08:28:09 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import Config from "./config";
 import * as graphics from "./graphics"
 
 class Window {
-    open() {
-
+    async open() {
+        const ret:[cp.ExecException|null, string, string] = await new Promise(resolve => {
+            cp.exec('./LOVE/love.exe ./love-src', (err, stdout, stderr) => resolve([err, stdout, stderr]));
+        });
+        const err = ret[0]
+        const stdout = ret[1]
+        const stderr = ret[2]
+        console.log({err, stdout, stderr})
     }
-    close() {
+    async close() {
 
     }
     config = new Config()
